6faa3c33488f8da3111599f97ab8b42a131bccdf
neha111993 <sonineha1330@gmail.com>
2026-01-22 21:34:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "D";
+    } else {
+        this.textContent = "L";
+    }
+    console.log('current class name: ' + className);
+});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
